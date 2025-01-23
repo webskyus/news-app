02404564ac373b7d4b504f217bb65f43820fdd67
webskyus <webskyus@gmail.com>
2025-01-23 11:05:19 +0400

    Add home page structure:
    Header
    Tags
    Highlight Posts
    Popular Posts
    Football Calendar
    New Posts
    Latest Videos
    Trendy Posts
    Weather Info
    Top Posts
    Footer

---
+++ src/widgets/football-calendar/ui/football-calendar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FootballCalendar = () => {
+	return <section>FOOTBALL CALENDAR</section>;
+};
+
+export { FootballCalendar };
---
+++ src/widgets/highlight-posts/ui/highlight-posts.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HighlightPosts = () => {
+	return <section>Highlights</section>;
+};
+
+export { HighlightPosts };
