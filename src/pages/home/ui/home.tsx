f9bd7bc7dc51794958a0076cf92f0bcdf9d63696
webskyus <webskyus@gmail.com>
2025-01-23 10:33:08 +0400

    work with tailwind | add base styles

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-	return <div>TEST</div>;
+const Home = () => {
+	return <>
+		<p className={'font-medium'}>TEST TITLE 1</p>
+		<p>TEST TITLE 2</p>
+	</>;
+}
+
+export {
+	Home
 }
