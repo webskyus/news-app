b705d213b527fd7a7aa6baea4fdf043817a17546
webskyus <webskyus@gmail.com>
2025-01-29 11:37:37 +0400

    add post card templates

--- src/widgets/highlight-posts/ui/highlight-posts.tsx
+++ src/widgets/highlight-posts/ui/highlight-posts.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Post } from '@entities/post';
+import {POST_CARD_TEMPLATE} from "@entities/post/ui/post";
 
 const HighlightPosts = () => {
-	return <section>Highlights</section>;
+	return (
+		<section>
+			<Post template={POST_CARD_TEMPLATE.POST_CARD_HIGHLIGHT} meta={false} />
+		</section>
+	);
 };
 
 export { HighlightPosts };
